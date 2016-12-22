a849986b93582c44cf01271e4aa6933579586ba4
Kelsey Robertson <krobertsonsja@gmail.com>
2016-12-21 20:52:28 -0500

    Initial Commit for first file to code.

---
+++ BasicTraining/src/Intro/Intro.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package Intro;
+
+/**
+ * @author kelseyr
+ *
+ */
+public class Intro {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
