a0ded4b81169ff696bc026a0aa7b2e1e47fcdb5d
Kelsey Robertson <krobertsonsja@gmail.com>
2016-12-21 22:16:17 -0500

    Sales Tax from algorithm workbench.

--- BasicTraining/src/Intro/Intro.java
+++ BasicTraining/src/Intro/Intro.java
@@ -15,9 +15,24 @@ public class Intro {
 	public static void main(String[] args) {
 		getAvailableCredit(2500, 30);
 		getAvailableCredit(50, 60);
+		
+		getSalesTax(15.99, 7);
+		getSalesTax(320.81, 10);
 
 	}
 	
+	private static void getSalesTax(double retailPrice, double percent) {
+		double totalSale = 0.00;
+		double salesTaxTotal = retailPrice * (percent/100);
+		totalSale = retailPrice + salesTaxTotal;
+		
+		System.out.println("Retail Price: " + retailPrice);
+		System.out.println("Sales Tax Percentage: " + percent + "%");
+		System.out.println("Sales Tax Total: " + String.format("%.2f", salesTaxTotal));
+		System.out.println("Total for Purchase: " + String.format("%.2f",totalSale));
+		System.out.println();
+	}
+
 	private static void getAvailableCredit(int maxCredit, int usedCredit){
 		int availableCredit = maxCredit - usedCredit;
 		System.out.println("Maximum Credit: " + maxCredit);
