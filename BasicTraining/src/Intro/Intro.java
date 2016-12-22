ed95f4ae5d6bd062ac6bb37c0d16163041c4bf35
Kelsey Robertson <krobertsonsja@gmail.com>
2016-12-21 21:53:07 -0500

    Algorithm Workbench
    Very basic available credit checking function based off book Starting
    out with C++ from control stuctures through objects.

--- BasicTraining/src/Intro/Intro.java
+++ BasicTraining/src/Intro/Intro.java
@@ -13,8 +13,22 @@ public class Intro {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		getAvailableCredit(2500, 30);
+		getAvailableCredit(50, 60);
 
+	}
+	
+	private static void getAvailableCredit(int maxCredit, int usedCredit){
+		int availableCredit = maxCredit - usedCredit;
+		System.out.println("Maximum Credit: " + maxCredit);
+		System.out.println("Credit Used: " + usedCredit);
+		if(availableCredit < 0){
+			System.out.println("OVER CREDIT LIMIT! " + availableCredit + " over the credit limit");
+			System.out.println();
+		}else{
+			System.out.println("Available Credit for Use: " + availableCredit);
+			System.out.println();
+		}
 	}
 
 }
